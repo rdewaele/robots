8c82edae1607835888fb23377ddc17964e1a0db8
Robrecht Dewaele <robrecht.dewaele@gmail.com>
2012-04-10 11:11:19 +0200

    little silly script to automate documentation downloading because putting documentation in this repo seems to be a waste of space, and it doesn't look like it's going to disappear soon from the host

---
+++ hardware/kheperaIII/get_usermanuals.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# A little silly script to quickly download the (supposedly) latest manuals for
+# the Khepera III and Korebot II.
+wget 'http://ftp.k-team.com/KheperaIII/UserManual/Kh3.Robot.UserManual.pdf'
+wget 'http://ftp.k-team.com/KorebotII/UserManual/KorebotII_UserManual.pdf'
