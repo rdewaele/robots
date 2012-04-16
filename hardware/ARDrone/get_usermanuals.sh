fa02f179b9f24162d55487ffd1b90d216869f626
Robrecht Dewaele <rdewaele@vub.ac.be>
2012-04-16 13:51:20 +0200

    initial information about the Parrot AR Drone

---
+++ hardware/ARDrone/get_usermanuals.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# A little silly script to quickly download the (supposedly) latest manuals for
+# the Khepera III and Korebot II.
+wget 'http://www.parrot.com/fr/support/guidesutilisateur/ar.drone_user-guide_uk.pdf'
